99944df4378e218d5e8fca19335134836e63a650
Angurio meloni <masserini.riccardo.studente@itispaleocapa.it>
2023-01-13 12:53:49 +0100

    first

--- Funzioni array ripasso/Program.cs
+++ Funzioni array ripasso/Program.cs
@@ -11,12 +11,25 @@ namespace Funzioni_array_ripasso
         static void Main(string[] args)
         {
             //dichiarazioni
+            int n = 0;
             int[] array1 = new int[100];
-            Carray();
+            Console.WriteLine("inserisci la dimensione dell'array");
+            n=int.Parse(Console.ReadLine());
+            ArrayC(n, array1);
+            OutputH(array1, n);
         }
-        void Carray()
+        static void ArrayC(int n, int[] array1)
         {
-
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = r.Next(10);
+            }
         }
+        static void OutputH(int[] array1,int n)
+        {
+            
+        }
+
+    }     
     }
-}
