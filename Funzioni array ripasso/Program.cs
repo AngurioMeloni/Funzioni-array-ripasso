f1e10d74b9e78a2c61f07c4826cc53f2831bad81
Masserini.20193 <Masserini.20193@LABI2-13PC08>
2023-01-13 12:30:10 +0100

    Aggiungere i file di progetto.

---
+++ Funzioni array ripasso/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funzioni_array_ripasso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dichiarazioni
+            int[] array1 = new int[100];
+            Carray();
+        }
+        void Carray()
+        {
+
+        }
+    }
+}
